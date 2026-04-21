56c734904d014fe117dae78c75152910bb7cfd9a
r3dbars <r3dbars@users.noreply.github.com>
2026-04-21 16:30:40 -0500

    Refresh release metadata for rebuilt 1.1.15 artifact

--- Casks/transcripted.rb
+++ Casks/transcripted.rb
@@ -1,6 +1,6 @@
 cask "transcripted" do
   version "1.1.15"
-  sha256 "29ccdc6475c42e13566e5e5734f1e18cd37205dbd0542d92b86a29dcdc66be7f"
+  sha256 "5f5977394c22ac03465b2205214d57a48d09dae50cca3a10481b8fbbaee6793b"
 
   url "https://github.com/r3dbars/transcripted/releases/download/v#{version}/Transcripted-#{version}.dmg",
       verified: "github.com/r3dbars/transcripted/"
