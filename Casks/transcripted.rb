43ad518402af9577b4863d56495135adc32726c7
r3dbars <r3dbars@users.noreply.github.com>
2026-04-29 09:36:29 -0500

    Update release metadata for 1.1.26

--- Casks/transcripted.rb
+++ Casks/transcripted.rb
@@ -1,6 +1,6 @@
 cask "transcripted" do
-  version "1.1.25"
-  sha256 "0a91536b468c96db9d234dfb3d17e9c2c0c28b95637882857e4ca65193dae72d"
+  version "1.1.26"
+  sha256 "14093c55403e7d88aa074e5a5f0ee4293261d81e322fd1a56a8958dac3c5319b"
 
   url "https://github.com/r3dbars/transcripted/releases/download/v#{version}/Transcripted-#{version}.dmg",
       verified: "github.com/r3dbars/transcripted/"
