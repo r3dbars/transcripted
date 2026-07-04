ec09e2021bdb3169e559e099397c3c5fc794cb71
r3dbars <r3dbars@users.noreply.github.com>
2026-07-04 07:53:55 -0500

    Classify audio engine start timeouts

--- Sources/Speech/ParakeetEngine.swift
+++ Sources/Speech/ParakeetEngine.swift
@@ -2349,7 +2349,7 @@ class ParakeetEngine: ObservableObject {
                 }
             } catch {
                 let operationTimedOut = error is ParakeetAudioEngineWorkError
-                let context = audioStartContext(
+                var context = audioStartContext(
                     attempt: attempt,
                     isRecoveryAttempt: isRecoveryAttempt,
                     engineWasRunning: snapshot.engineWasRunning,
@@ -2358,8 +2358,10 @@ class ParakeetEngine: ObservableObject {
                     error: error
                 )
                 let failureReason = operationTimedOut
-                    ? ParakeetStartRecordingFailureReason.audioEngineStartFailed
+                    ? ParakeetStartRecordingFailureReason.audioEngineStartTimedOut
                     : ParakeetAudioFormatReadinessPolicy.startFailureReason(for: error as NSError)
+                context["failure_kind"] = operationTimedOut ? "audio_engine_start_timeout" : "audio_engine_start_failed"
+                context["sample_flow_started"] = "\(didReceiveAudioSamples)"
                 let shouldRetry = !operationTimedOut
                     && failureReason == .audioEngineStartFailed
                     && ParakeetAudioStartRecoveryPolicy.shouldRetryStartFailure(
@@ -2420,7 +2422,7 @@ class ParakeetEngine: ObservableObject {
                         context: context
                     )
                     let startFailureAction = ParakeetStartRecordingFailurePolicy.action(
-                        for: .audioEngineStartFailed,
+                        for: .audioEngineStartTimedOut,
                         isRecoveryAttempt: isRecoveryAttempt
                     )
                     if startFailureAction.markFormatUnready {
