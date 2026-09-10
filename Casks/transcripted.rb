f91755663d7a4ad97712d51dd2200dbef2e61ce2
r3dbars <r3dbars@users.noreply.github.com>
2026-09-10 13:00:43 -0500

    Publish 1.1.59 update metadata

--- Casks/transcripted.rb
+++ Casks/transcripted.rb
@@ -1,6 +1,6 @@
 cask "transcripted" do
-  version "1.1.58"
-  sha256 "fcbe42567ddd3cd64456cfd2a99b5ca4b1dcc4c025223152cb71c5783d97021a"
+  version "1.1.59"
+  sha256 "e245f148b2aa19437624b198d09f9cb18621fbd63909399fbfba45f395d0a09c"
 
   url "https://github.com/r3dbars/transcripted/releases/download/v#{version}/Transcripted-#{version}.dmg",
       verified: "github.com/r3dbars/transcripted/"
