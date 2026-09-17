5785b52b9a72ea8302c3304ea3ddf68c1b6a6482
Justin Betker <tz427gsydr@privaterelay.appleid.com>
2026-09-17 14:39:50 -0500

    Prepare Transcripted 1.1.61 signed update metadata (#1745)
    * Prepare Transcripted 1.1.61 release candidate
    
    * Prepare verified Transcripted 1.1.61 update metadata
    
    ---------
    
    Co-authored-by: r3dbars <r3dbars@users.noreply.github.com>

--- Casks/transcripted.rb
+++ Casks/transcripted.rb
@@ -1,6 +1,6 @@
 cask "transcripted" do
-  version "1.1.60"
-  sha256 "c911bad7c53d6441a0ca4a8742d66c9970fbe0895ed537fd3d6d407c154804a4"
+  version "1.1.61"
+  sha256 "88ab813ea7e5721a9a453cdfc5ecefe846f0358988b1bf0f4f5418702bc2d9fa"
 
   url "https://github.com/r3dbars/transcripted/releases/download/v#{version}/Transcripted-#{version}.dmg",
       verified: "github.com/r3dbars/transcripted/"
